876d162b832918ce143fdff7220875d1f3bfff91
zearwat <pongniwat@365zocial.com>
2017-11-11 20:58:16 +0700

    Update Button

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ if (!is_null($events['events'])) {
                     } else if ($receiveText == '3') {
 
                         $actions = array (
-                            New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("yes", "1"),
-                            New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("no", "2")
+                            New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("yes", "text 1"),
+                            New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("no", "text 2")
                         );
 
                         $buttonBuilder = new TemplateBuilder\ConfirmTemplateBuilder('confirm message',$actions);
